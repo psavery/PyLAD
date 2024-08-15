25a519d1bda96718c0d84c9e3908de10fc622b19
Patrick Avery <patrick.avery@kitware.com>
2024-08-15 13:37:36 -0500

    Convert path to string for Windows
    Otherwise, windows will not be happy...
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- pyxisl/utils/load_xisl.py
+++ pyxisl/utils/load_xisl.py
@@ -7,7 +7,7 @@ from pyxisl.utils.xisl_paths import path_to_xisl_library
 def load_xisl() -> CDLL:
     # First, load the DLL
     path = path_to_xisl_library()
-    lib = CDLL(path.resolve())
+    lib = CDLL(str(path.resolve()))
 
     # Add the ctypes wrappers
     # These add some safety features to the ctypes accesses, including checking
